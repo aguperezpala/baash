6d242c4a7ded4738990ba881507acac6b0a689ea
Hobborg <Hobborg@35fc8e4e-0055-0410-9326-19aa9d9376bb>
2008-08-23 05:47:02 +0000

    Estuve trabajando con el command.c del Agus. Cambié muchas cosas, pero no hubo cambios muy profundos. Lean especialmente el scommand_to_string

--- command.c
+++ command.c
@@ -11,11 +11,14 @@
 struct scommand_s
 {
 	bool builtin; 
-	GQueue * args; /*argumentos y nombre del representado por una cola*/
+	GQueue * args; /* nombre del comando y sus argumentos, se usa una cola */
 	bstring dirOut;
 	bstring dirIn;
 };
 
+#define BASE_ALLOC 1
+
+/*** Inicialización y destrucción ***/
 
 scommand *scommand_new (void)
 {
@@ -24,17 +27,19 @@ scommand *scommand_new (void)
 	result = (scommand *) calloc (1,sizeof (struct scommand_s) );
 	assert (result != NULL);
 	
-	result->builtin=false;
-	result->args=g_queue_new (); /*generamos la cola vacia*/
+	/* por defecto se inicializa todo comando como "no interno" */
+	result->builtin = false;
+	/*generamos la cola vacia*/
+	result->args = g_queue_new ();
 	
 	assert (result->args != NULL); /*nos aseguramos que se haya creado*/
 	
 	result->dirOut = NULL;
 	result->dirIn = NULL;
 	
-	
-	
-	/*assert( COMPLETAR********************************************/
+	/* ENSURES */
+	assert (result != NULL);
+	assert (g_queue_is_empty (result->args));
 	
 	return result;
 }
@@ -42,84 +47,100 @@ scommand *scommand_new (void)
 void scommand_destroy (scommand *self)
 {
 	bstring aux = NULL;
-	
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	bdestroy (self->dirOut);
 	bdestroy (self->dirIn);
 	
-	assert (self->args!=NULL); /*evitamos que nunca haya sido creado*/
-	
-	while(!g_queue_is_empty (self->args))
-	{
-		aux=(bstring)g_queue_pop_head (self->args);
-		bdestroy (aux);
+	if (self->args != NULL) {
+		/* si hay argumentos hay que liberarlos antes de destruir la cola */
+		while(!g_queue_is_empty (self->args)) {
+			aux = g_queue_pop_head (self->args);
+			bdestroy (aux);
+		}
+		g_queue_free (self->args);
 	}
 	
-	g_queue_free (self->args);
-	
-	free(self);
+	free (self);
 }
 
+/*** Modificadores ***/
 
 void scommand_push_back (scommand *self, bstring argument)
-{
+{	/* encolamos por detrás :D */
+	/* REQUIRES */
 	assert (self != NULL);
 	assert (argument != NULL);
 	
-	g_queue_push_tail (self->args, argument); /*encolamos por detras*/
+	g_queue_push_tail (self->args, argument);
 	
+	/* ENSURES */
 	assert (!g_queue_is_empty (self->args));
 }
 
 
 void scommand_push_front (scommand *self, bstring argument)
-{	
+{	/* encolamos por adelante */
+	/* REQUIRES */
 	assert (self != NULL);
 	assert (argument != NULL);
 	
-	g_queue_push_head (self->args, argument); /*encolamos por delante*/
+	g_queue_push_head (self->args, argument);
 	
+	/* ENSURES */
 	assert (!g_queue_is_empty (self->args));
 }
 
 void scommand_pop_front (scommand *self)
-{	
-	bstring aux=NULL;
-	
+{	/* removemos el primer elemento */
+	bstring aux = NULL;
+	/* REQUIRES */
 	assert (self != NULL);
-	
-	aux = g_queue_pop_tail (self->args);
-	bdestroy(aux);
-	
 	assert (!g_queue_is_empty (self->args));
+	
+	aux = g_queue_pop_head (self->args);
+	bdestroy (aux);
 }
 
 
 void scommand_set_builtin (scommand *self, bool builtin)
-{
+{	/* marca al comando simple como interno o no interno */
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	self->builtin = builtin;
 }
 
 void scommand_set_redir_in (scommand *self, bstring filename)
-{
+{	/* setea con 'filename' la redireccion de entrada */
+	/* REQUIRES */
 	assert (self != NULL);
 	
-	bassign (self->dirIn, (const_bstring) filename);
-}
-void scommand_set_redir_out (scommand *self, bstring filename)
-{
-	assert (self != NULL);
-	
-	bassign (self->dirOut, (const_bstring) filename);
+	if (filename != NULL && blength (filename)>0 ) {
+		bassign (self->dirIn, (const_bstring) filename);
+	} else {
+		self->dirIn = bfromcstr ("");
+	}
 }
 
-/* Proyectores */
+void scommand_set_redir_out (scommand *self, bstring filename)
+{	/* setea con 'filename' la redireccion de salida */
+	/* REQUIRES */
+	assert (self != NULL);
+
+	if (filename != NULL && blength (filename)>0 ) {
+		bassign (self->dirIn, (const_bstring) filename);
+	} else {
+		self->dirIn = bfromcstr ("");
+	}
+}
+
+/*** Proyectores ***/
 
 bool scommand_is_empty (const scommand *self)
-{
+{	/* chequea si el scommand está vacío (no hay comando ni argumentos) */
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	return g_queue_is_empty (self->args);
@@ -127,79 +148,104 @@ bool scommand_is_empty (const scommand *self)
 }
 
 unsigned int scommand_length (const scommand *self)
-{
+{	/* devuelve la cantidad de comandos + argumentos del scommand */
+	/* REQUIRES */
 	assert (self != NULL);
 	
-	return (unsigned int) g_queue_get_length (self->args);
+	if (!scommand_is_empty (self)) {
+		return (unsigned int) g_queue_get_length (self->args);
+	} else {
+		return (unsigned int) 0;
+	}
 }
 
 bstring scommand_front (const scommand *self)
-{
+{	/* devuelve una copia del primer elemento de la cola */
 	bstring result = NULL;
-	
+	/* REQUIRES */
 	assert (self != NULL);
 	assert (!scommand_is_empty (self));
 	
-	result = (bstring) g_queue_peek_head (self->args);
-	/* esta funcion nos devuelve el puntero al primer elemento de la cola */
+	result = bstrcpy ((const_bstring) g_queue_peek_head (self->args));
 	
+	/* ENSURES */
 	assert (result != NULL);
 	
 	return result;
 }
 
 bool scommand_get_builtin (const scommand *self)
-{
+{	/* ¿self es un comando interno? */
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	return self->builtin;
 }
 
 bstring scommand_get_redir_in (const scommand *self)
-{
+{	/* obtiene el nombre de la redireccion de entrada */
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	return self->dirIn;
-	/* como se ve en la implementacion si o si dirIn se guarda un dato o
-	   NULL en caso contrario, por lo que no hace falta comprobarlo     */
+	/* como se ve en la implementacion, si o si dirIn se guarda un dato o
+	 * NULL en caso contrario, por lo que no hace falta comprobarlo 
+	 */
 }
 	
 	
 bstring scommand_get_redir_out (const scommand *self)
-{
+{	/* obtiene el nombre de la redireccion de salida */
+	/* REQUIRES */
 	assert (self != NULL);
 	
 	return self->dirOut;
 }
 
 bstring scommand_to_string (const scommand *self)
-{
-	bstring result = NULL;
-	unsigned int i = 0; 
-	
+{	/* serializador del scommand, para debuggeo solamente
+	 * imprime el comando de forma legible para el ojo humano
+	 * si el comando es vacío devuelve una cadena vacía de BASE_ALLOC lugares
+	 */
+	bstring result = NULL , aux = NULL;
+	unsigned int n = scommand_length (self);
+	int i = 0;
+	/* REQUIRES */
 	assert (self != NULL);
 	
-	if (!balloc (result,1)) /*se produjo un error*/
-		return NULL;
+	result = bfromcstralloc (BASE_ALLOC,"");
 	
-	i = scommand_length (self);
-	while (i > 0)
-	{
-		/*COMPLETAR*/
+	/* si hay comandos o argumentos en el scommand los concatenaremos 
+	 * de a uno con result, con una separacion de \t entre ellos
+	 */
+	while ((unsigned int) i < n) {
+		/* método forzado por el casteo 'const' del parámetro
+		 * hay problemas con los tipos de lo devuelto por
+		 * q_queue_peek_nth y lo tomado por bstrcpy
+		 */
+		aux = bstrcpy ((const_bstring) g_queue_peek_nth (self, i+1));
+		bcatcstr (result, "\t");
+		bconcat (result, (const_bstring) aux);
+		bdestroy (aux);
+		i++;
 	}
+	/*	aux = scommand_front (self);
+		scommand_pop_front (self);
+		bcatcstr (result, (const char *) "\t");
+		bconcat (result, (const_bstring) aux);
+		scommand_push_back (self, aux);
+		i++;
+		
+		Forma sugerida en el .h para tocar cosas del scommand
+		El compilador se queja porque tocamos el parámetro de
+		 la función, que esta castedo con 'const'
+		Si se elimina ese casteo esta versión funciona bien, 
+		 y tiene la ventaja de usar funciones ya definidas.
+	}*/
+	
+	/* ENSURES */
+	assert (scommand_is_empty (self) || (blength (result)>0));
 	
 	return result;
 }
-	
-	
-/* Preety printer para hacer debugging/logging.
- * Genera una representación del comando simple en un string (aka "serializar")
- *   self: comando simple a convertir
- *   Returns: un string con la representación del comando simple similar
- *     a lo que se escribe en un shell
- * Requires: self!=NULL
- * Ensures: scommand_is_empty(self) ||
- *   scommand_get_redir_in(self)==NULL || scommand_get_redir_in(self)==NULL ||
- *   blength(result)>0
- */
-	
+
