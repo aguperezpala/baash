f2057f192986dda022b5f5ac0dd67132a4940ecb
Hobborg <Hobborg@35fc8e4e-0055-0410-9326-19aa9d9376bb>
2008-08-25 22:36:36 +0000

    Agregué un mini main para testear los pipelines. Logré hacer que la mayoría de las funciones actúen y devuelvan las cosas de acuerdo a los test. Estamos al 97%, pero hay más leaks que la mierda. ¡Cuidado!, creo que la mayoría de los leaks aparecieron cuando compatibilicé las funciones con los test, así que cuiden de no cagarla cuando los arreglen. Lean bien los *_destroy.

--- scommand_test.c
+++ scommand_test.c
@@ -2,11 +2,11 @@
 #include <assert.h>
 #include "command.h"
 
-
+/*
 int main (void)
 {
 	scommand *scmd = NULL;
-	bstring str1 = NULL , str2 = NULL, aux = NULL;
+	bstring str1 = NULL, str2 = NULL, str3 = NULL, str4 = NULL, aux = NULL;
 	bool b = false;
 
 	scmd = scommand_new ();
@@ -18,6 +18,7 @@ int main (void)
 
 	aux = scommand_front (scmd);
 	b = biseq (aux, str1);
+	aux = NULL;
 	scommand_pop_front (scmd);
 	aux = scommand_front (scmd);
 	b = b && biseq (aux, str2);
@@ -28,13 +29,36 @@ int main (void)
 	else
 		printf ("FAIL <-- Test scommand_to_string 'mismo bstring'\n");
 
+	aux = NULL;
+	scommand_set_redir_in (scmd, str1);
+	aux = scommand_get_redir_in (scmd);
+
+	assert (biseq (aux, str1));
+	assert (scommand_get_redir_in (scmd) == aux);
+
+	printf ("PASS <-- Test scommand_set&get_redir_in\n");
+
+	scommand_push_front (scmd, str2);
+	scommand_push_front (scmd, str1);
+	str3 = bfromcstr ("archivo_salida");
+	scommand_set_redir_out (scmd, str3);
+	str4 = bfromcstr ("archivo_entrada");
+	scommand_set_redir_in (scmd, str4);
+	/ scmd = ( 123 456 < archivo_entrada > archivo_salida) /
+	
+	aux = scommand_to_string (scmd);
+	assert (aux != NULL);
+	printf ("scmd = %s\n", aux->data);
+	printf ("scmd = %s\n", aux->data);
+
+	scommand_destroy (scmd);
 	bdestroy (str1);
 	bdestroy (str2);
+	bdestroy (str3);
+	bdestroy (str4);
 	bdestroy (aux);
-	assert (scommand_is_empty (scmd));
-	scommand_destroy (scmd);
 
 	return 0;
-}
+}*/
 
 
