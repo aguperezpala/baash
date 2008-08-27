78ccd447d737b398344cf040fc63c7c3024585c7
aguperezpala <aguperezpala@35fc8e4e-0055-0410-9326-19aa9d9376bb>
2008-08-27 19:46:43 +0000

    falta menos para terminar el parser :D

--- parser.c
+++ parser.c
@@ -6,8 +6,6 @@
 
 struct parser_s {
 	Lexer* lex; /*lexer para el parseo*/
-	pipeline* pipe; /*vamos a apuntar al ultimo pipeline =*/
-	scommand* scmd; /*scmd temporal*/
 	bstring strtmp; /*vamos a almacenar temporalmente lo obtenido del lexer*/
 	parser_state state; /*vamos a determinar 5 estados:
 			0->parar de leer = SALIR
@@ -17,6 +15,7 @@ struct parser_s {
 			4->pipeline
 			5->no_wait
 		   */
+	parser_error err;
 };
 
 parser* parser_new (void){
@@ -26,12 +25,12 @@ parser* parser_new (void){
 	result = (parser *) calloc (1, sizeof (struct parser_s));
 	
 	assert (result != NULL);
-		
+	
+	result->err = PARSER_NO_ERROR; /*no tenemos ningun error*/
 	result->lex = lexer_new (PARSER_IN);/*le pasamos "stdin"*/ 
 	/*se asegura lexer de que se haya creado*/
 	result->strtmp = NULL; /*no lo inicializamos*/
-	result->pipe = NULL;
-	result->state = STATE_EXIT;
+	result->state = PARSER_STATE_EXIT;
 	
 	return result;
 }
@@ -47,115 +46,160 @@ void parser_destroy (parser* self){
 	
 	free (self); /*tener en cuenta que el pipeline no quede inalcanzable*/
 }
-
-
-bool continue_reading (bstring endl){
-	bool result = false;
 	
-	if (endl != NULL)
-		result = (bstrchrp (endl, '\n', 0) == 0);
+
+parser_error parser_set_state (parser* self){
+	bstring aux = NULL;
+	parser_error result = PARSER_NO_ERROR;
+	/*en caso de que sea null o sea fin de linea terminamos*/
 	
+	assert (self != NULL);
+	
+	/*vamos a setear el state como "normal", osea como cmd siempre, ya que
+	 *de caso contrario automaticamente se setea el tipo correcto mas abajo*/
+	self->state = PARSER_STATE_CMD;
+	/*primero tomamos el caracter de "operador" que determina alguno de los
+	 * estados
+	*/
+	lexer_next_char (self->lex, PARSER_END_CMD);
+	
+	if (!lexer_is_off (self->lex))
+		if ((aux = lexer_item (self->lex)) == NULL){ /*tomamos el caracter*/
+			self->state = PARSER_STATE_EXIT;
+			return PARSER_ERROR;
+		}
+	
+	if (bstrchrp (aux, '\n', 0) == 0){
+		self->state = PARSER_STATE_EXIT;
+		result = PARSER_NO_ERROR;
+	}else if (bstrchrp (aux, '>', 0) == 0){
+		self->state = PARSER_STATE_DIR_OUT;
+		result = PARSER_NO_ERROR;
+	}else if (bstrchrp (aux, '<', 0) == 0){
+		self->state = PARSER_STATE_DIR_IN;
+		result = PARSER_NO_ERROR;
+	}else if (bstrchrp (aux, '|', 0) == 0){
+		self->state = PARSER_STATE_PIPE;
+		result = PARSER_NO_ERROR;
+	}else if (bstrchrp (aux, '&', 0) == 0){
+		self->state = PARSER_STATE_NO_WAIT;
+		result = PARSER_NO_ERROR;
+	}
+	
+	
+	bdestroy (aux);
+	/*si no entro en ninguna de esas es porque hay un espacio en blanco
+	 *por lo que tenemos que seguir leyendo la cadena de caracteres */
+	return result;
+}
+
+bstring parser_get_bstrcmd (parser* self){
+	bstring result = NULL;
+	
+	assert (self != NULL);
+	
+	/*tenemos que tener en cuenta que cada vez que vamos a usar alguna func.
+	 *del lexer tenemos que corroborar que este "prendido"
+	*/
+	/*primero vamos a limpiar los espacios en blanco*/
+	lexer_skip (self->lex, BLANK);
+	
+	/*ahora leemos el el "dato" si es posible*/
+	lexer_next_to (self->lex, PARSER_END_CMD);
+	
+	/*si es posible lo apuntamos en result*/
+	if (!lexer_is_off (self->lex))
+		result = lexer_item (self->lex);
+	
+	lexer_skip (self->lex, BLANK);
+	/*en caso de no poder obtener el dato devolvemos NULL*/
 	return result;
 }
 	
-/* no nos hace falta ningun "requerimiento" para llamar a esta funcion*/
-parser_state get_state (bstring endl){
 	
-	if (bstrchrp (endl, '\n', 0) == 0)
-		return STATE_EXIT;
-		
-	if (bstrchrp (endl, '>', 0) == 0)
-		return STATE_DIR_OUT;
-	
-	if (bstrchrp (endl, '<', 0) == 0)
-		return STATE_DIR_IN;
-	
-	if (bstrchrp (endl, '|', 0) == 0)
-		return STATE_PIPE;
-	
-	if (bstrchrp (endl, '&', 0) == 0)
-		return STATE_NO_WAIT;
-		
-	/*si no se da ninguno de los casos anteriores entonces estamos en cmd*/
-	return STATE_CMD;
-}
-	
-	
-/*ls -a -l -j > gran_mono | grep < fede*/
+
 
 pipeline* parse_pipeline (parser* self){
-	bstring aux = NULL;
+	pipeline * result = NULL;
+	scommand * scmd = NULL;
 	
 	assert (self != NULL);
 	/*seteamos el estado inicial que es de "command"*/
-	self->state = STATE_CMD;
-	self->scmd = scommand_new (); /*inicializamos el scmd y el pipe*/
-	self->pipe = pipeline_new ();
+	self->state = PARSER_STATE_CMD;
+	scmd = scommand_new (); /*inicializamos el scmd y el pipe*/
+	result = pipeline_new ();
 	
 	
 	
-	while (self->state != STATE_EXIT && self->state != STATE_NO_WAIT){
-		/*mientras tengamos que leer*/
-		/*borramos los "posibles" espacios en blanco anteriores*/
-		lexer_skip (self->lex, BLANK);
+	while (self->state != PARSER_STATE_EXIT && self->err == PARSER_NO_ERROR){
+		/*mientras tengamos que leer, y no se haya ningun error*/
 		
-		lexer_next_to (self->lex, PARSER_END_CMD);/*leemos*/
-		self->strtmp = lexer_item (self->lex); /*almacenamos temp*/
-		printf ("\nself->strtmp: %s\t",self->strtmp->data);
-		/*ahora obtenemos el ultimo caracter, y queda almacenado en
-		*lexer_item ();*/
-		lexer_next_char (self->lex, PARSER_END_CMD);
-		aux = lexer_item (self->lex);
-		printf ("aux: %s\t",aux->data);
-		/*ACA TENEMOS QUE CHEQUEAR QUE self->strtmp != NULL, creo quede
-		 * nunca sucede pero revisar mejor */
+		if ((self->strtmp = parser_get_bstrcmd (self)) == NULL){
+			/*salimos del ciclo, ya no hay mas nada que leer*/
+			break;
+		}
 		
 		/*este switch probablemente seria mejor con un arreglo de punteros
 		 *a funciones*/
 		switch (self->state){
 			
-			case STATE_CMD: /*metemos el cmd o argumento al scmd*/
-				scommand_push_back (self->scmd, self->strtmp);
+			case PARSER_STATE_CMD: /*metemos el cmd o argumento al scmd*/
+				scommand_push_back (scmd, self->strtmp);
 				break;
 			
-			case STATE_DIR_OUT:
-				scommand_set_redir_out (self->scmd, self->strtmp);
+			case PARSER_STATE_DIR_OUT:
+				/*aca podriamos tener en cuenta si ya hay un
+				redir_out seteado*/
+				if (blength (self->strtmp) == 0){ /*error*/
+					printf ("ERRORRRR NO OUT FILE");
+					self->err = PARSER_ERROR_DIR_OUT;
+				}
+				scommand_set_redir_out (scmd, self->strtmp);
 				break;
 				
-			case STATE_DIR_IN:
-				scommand_set_redir_in (self->scmd, self->strtmp);
+			case PARSER_STATE_DIR_IN:
+				if (blength (self->strtmp) == 0){ /*error*/
+					printf ("ERRORRRR NO IN FILE");
+					self->err = PARSER_ERROR_DIR_IN;
+				}
+				scommand_set_redir_in (scmd, self->strtmp);
 				break;
 			
-			case STATE_PIPE:
+			case PARSER_STATE_PIPE:
 				/*introducimos el scommand*/
-				pipeline_push_back (self->pipe, self->scmd);
-				self->scmd = scommand_new (); /*generamos nuevo*/
+				pipeline_push_back (result, scmd);
+				scmd = scommand_new (); /*generamos nuevo*/
 				/*y le introducimos la ultima info tomada*/
-				scommand_push_back (self->scmd, self->strtmp);
+				scommand_push_back (scmd, self->strtmp);
+				
 				break;
 				
-			case STATE_EXIT:
+			case PARSER_STATE_EXIT:
 				/*no deberia entrar nunca*/
 				break;
 			
-			case STATE_NO_WAIT:
-				/*no deberia entrar nunca*/
+			case PARSER_STATE_NO_WAIT:
+				pipeline_set_wait (result, false);
+				/*deberiamos hacer un nuevo scommand, pero sin
+				*esperar, ta complicado, seria un nuevo pipe*/
 				break;
 			
 		}
-		/*salteamos los espacios en blanco "posteriores"*/
-		lexer_skip (self->lex, BLANK);
-		printf ("state_ant: %d\t",self->state);
-		self->state = get_state (aux); /*obtenemos el estado "proximo"*/
-		printf ("state_post: %d\n",self->state);
-		bdestroy (aux); /*eliminamos aux*/
+		/*seteamos el proximo estado y en caso de error salimos*/
+		if (parser_set_state (self) == PARSER_ERROR)
+			break;
+			
+		
+		
+		
 	}
-	
-	if (self->scmd != NULL) /*agregamos el ultimo scmd hecho*/
-		pipeline_push_back (self->pipe, self->scmd);
+	if (self->state == PARSER_STATE_NO_WAIT)
+		pipeline_set_wait (result, false);
+	if (scmd != NULL) /*agregamos el ultimo scmd hecho*/
+		pipeline_push_back (result, scmd);
 	
 		
-	return self->pipe;
+	return result;
 }
 	
 	
