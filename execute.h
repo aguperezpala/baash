5dd681523e542f528e04e930a1fdbf9086c8a535
Hobborg <Hobborg@35fc8e4e-0055-0410-9326-19aa9d9376bb>
2008-09-01 03:40:38 +0000

    Execute con 3 funciones casi listas (ver el header) Faltan las funciones de ejecución de comandos built in, y las de cambio de ambiente (pasar el baash al 1º plano con su propio grupo de procesos) Además falta toda la lógica de no-espera de los procesos hijos, sin dejar zombies desparramados por doquier.
    --Thi line, and those below, will be ignored--
    
    M    execute.c
    M    execute.h

--- execute.h
+++ execute.h
@@ -6,11 +6,25 @@
 /* Ejecución de todos los comandos de una pipeline
  * RETURNS:
  *	El entero devuelto indica la correcta/incorrecta ejecución: 
- * si es 0 todos los comandos terminaron bien; si es 1 ocurrió algún
- * error que será impreso por stderr
+ * si es 0 todos los comandos terminaron bien
  * REQUIRES:
  *	pipe != NULL
- * ENSURES:
- *	¡¡¡ Lots of happyness !!!
  */
-int exec_pipe (pipeline *spipe , unsigned int spipe_len);
+int exec_pipe (pipeline *spipe);
+
+
+/* Ejecución in-situ de un comando no interno
+ * cmd_no --> posición del comando dentro de su pipeline
+ * pipes_no --> total de pipes que serán creadas para el
+ *              pipeline donde el comando reside
+ * pipe_fd --> todas las pipes de dicho pipeline
+ * scmd_len --> largo del comando a ejecutar
+ * REQUIRES:
+ *	scmd != NULL;
+ */
+void exe_cmd_nbin (scommand *scmd, unsigned int scmd_len, int **pipe_fd,
+		   unsigned int pipes_no, unsigned int cmd_no);
+
+
+
+#endif
