21993997b25e0a156a40f1a09730892a8d1bba82
Hobborg <Hobborg@35fc8e4e-0055-0410-9326-19aa9d9376bb>
2008-09-04 03:03:12 +0000

    Estoy pasando los nombres de variables al protocolo que siguen ustedes. Además creo que ya cacé como esperar o no esperar a los procesos hijos. Trabajando en ello.

--- trunk/execute.h
+++ trunk/execute.h
@@ -8,28 +8,32 @@
 
 
 /* Ejecución de todos los comandos de una pipeline
- * RETURNS:
- *	El entero devuelto indica el resultado de la ejecución: 
- * si es 0 todos los comandos terminaron bien, si es 10 se proveyó
- * una instrucción "exit" y hay que cerrar el bash. Cualquier otro
- * entero indica un error de ejecución.
  * REQUIRES:
  *	pipe != NULL
+ * RETURNS:
+ *	El entero devuelto indica el resultado de la ejecución: 
+ * si es 0 todos los comandos terminaron bien, si es EXIT se proveyó
+ * una instrucción "exit" y hay que cerrar el bash. Cualquier otro
+ * entero indica un error de ejecución.
+ * ENSURES:
+ *	El pipeline *spipe no es modificado
  */
 int exec_pipe (pipeline *spipe);
 
 
 /* Ejecución in-situ de un comando no interno
- * cmd_no --> posición del comando dentro de su pipeline
- * pipes_no --> total de pipes que serán creadas para el
- *              pipeline donde el comando reside
- * pipe_fd --> todas las pipes de dicho pipeline
- * scmd_len --> largo del comando a ejecutar
+ * cmdN --> posición del comando dentro de su pipeline
+ * pipesToll --> total de pipes que serán creadas para el
+ *               pipeline donde el comando reside
+ * pipe_FD -->  arreglo con los file descriptors de las '|'
+ * cmdLen --> largo del comando a ejecutar
  * REQUIRES:
  *	scmd != NULL;
+ * ENSURES:
+ * 	El comando *scmd no es modificado
  */
-void exe_cmd_nbin (scommand *scmd, unsigned int scmd_len, int **pipe_fd,
-		   unsigned int pipes_no, unsigned int cmd_no);
+void exe_cmd_nbin (scommand *scmd, unsigned int cmdLen, int **pipeFD,
+		   unsigned int pipesToll, unsigned int cmdN);
 
 
 /* Ejecución de un comando interno. Lo debe ejecutar directamente
@@ -40,6 +44,8 @@ void exe_cmd_nbin (scommand *scmd, unsigned int scmd_len, int **pipe_fd,
  *	El entero devuelto es: 0 en caso de correcta ejecución o
  * ausencia de ella; EXIT en caso de haber recibido una señal de exit;
  * cualquier otro  entero en caso de error.
+ * ENSURES:
+ *	El comando *scmd no es modificado
  */
 int exe_cmd_bin (scommand *scmd);
 
